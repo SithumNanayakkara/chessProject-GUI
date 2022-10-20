5b5bb1f0b2c8904b5f7c3ca4ea82e75045fcadb1
Sithum <115341061+SithumNanayakkara@users.noreply.github.com>
2022-10-20 13:23:56 +1300

    Added two classes to improve SOLID principles in the code - TODO

---
+++ src/ChessCommon/ChessData.java
@@ -0,0 +1,17 @@
+/*
+ * 
+ * Author: Sithum Nanayakkara
+ */
+package ChessCommon;
+                            //TO BE IMPLEMENTED
+/**
+ *
+ * @author Sithum Nanayakkara <fhb7119@autuni.ac.nz>
+ */
+public class ChessData 
+{
+    boolean loginSuccessful = false;
+    boolean quitGame = false;
+    
+    
+}

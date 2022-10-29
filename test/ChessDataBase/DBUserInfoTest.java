d9ea280f47deec92933d1662a31f08c17e05b71d
Sithum <115341061+SithumNanayakkara@users.noreply.github.com>
2022-10-29 18:38:20 +1300

    added DB jtest unit

---
+++ test/ChessDataBase/DBUserInfoTest.java
@@ -0,0 +1,157 @@
+/*
+ * 
+ * Author: Sithum Nanayakkara
+ */
+package ChessDataBase;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Sithum Nanayakkara <fhb7119@autuni.ac.nz>
+ */
+public class DBUserInfoTest {
+    
+    private DBUserInfo DBUser;
+    
+    
+    public DBUserInfoTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getInstance method, of class DBUserInfo.
+     */
+    @Test
+    public void testGetInstance() {
+        System.out.println("getInstance");
+        DBUserInfo expResult = DBUserInfo.getInstance();
+    }
+
+    /**
+     * Test of setupChessDB method, of class DBUserInfo.
+     */
+    @Test
+    public void testSetupChessDB() {
+        System.out.println("setupChessDB");
+        DBUserInfo instance = new DBUserInfo();
+        instance.setupChessDB();
+    }
+
+    /**
+     * Test of checkExistedTable method, of class DBUserInfo.
+     */
+    @Test
+    public void testCheckExistedTable() {
+        System.out.println("checkExistedTable");
+        String name = "ChessUserInfo";
+        DBUserInfo instance = new DBUserInfo();
+        boolean expResult = false;
+        boolean result = instance.checkExistedTable(name);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of loginUser method, of class DBUserInfo.
+     */
+    @Test
+    public void testLoginUserPass() {
+        System.out.println("loginUser");
+        String userName = "Sithum";
+        String password = "sithum";
+        DBUserInfo instance = new DBUserInfo();
+        boolean expResult = true;
+        boolean result = instance.loginUser(userName, password);
+        assertEquals(expResult, result);
+    }
+    
+
+    /**
+     * Test of loginUser method, of class DBUserInfo. Test when details are entered wrong
+     */
+    @Test
+    public void testLoginUserFail() {
+        System.out.println("loginUser");
+        String userName = "wrongusername";
+        String password = "wrongpassword";
+        DBUserInfo instance = new DBUserInfo();
+        boolean expResult = false;
+        boolean result = instance.loginUser(userName, password);
+        assertEquals(expResult, result);
+    }
+    
+     /**
+     * Test of loadUser method, of class DBUserInfo. When details are correct
+     */
+    @Test
+    public void testLoadUser() {
+        System.out.println("loadUser");
+        String userName = "Sithum";
+        String password = "sithum";
+        DBUserInfo instance = new DBUserInfo();
+        User expResult = new User("Sithum","sithum","sithum_nanayakkara@outlook.com",60);
+        User result = instance.loadUser(userName, password);
+        assertEquals(expResult.getUserName(), result.getUserName());
+        assertEquals(expResult.getEmail(), result.getEmail());
+        assertEquals(expResult.getPassword(), result.getPassword());
+        assertEquals(expResult.getScore(), result.getScore());
+    }
+    
+    /**
+     * Test of registerUser method, of class DBUserInfo.
+     * WILL FAIL IF RUN MORE THAN ONCE! WHICH IS EXPECTED
+     */
+    @Test
+    public void testRegisterUser() {
+        System.out.println("registerUser");
+        String userName = "test";
+        String email = "test";
+        String password = "test";
+        DBUserInfo instance = new DBUserInfo();
+        boolean expResult = true;
+        boolean result = instance.registerUser(userName, email, password);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of closeConnections method, of class DBUserInfo.
+     */
+    @Test
+    public void testCloseConnections() {
+        System.out.println("closeConnections");
+        DBUserInfo instance = new DBUserInfo();
+        instance.closeConnections();
+    }
+
+    /**
+     * Test of updateScore method, of class DBUserInfo.
+     */
+    @Test
+    public void testUpdateScore() {
+        System.out.println("updateScore");
+        User user = new User("test","test","test",10);
+        DBUserInfo instance = new DBUserInfo();
+        instance.updateScore(user);
+    }
+    
+}
